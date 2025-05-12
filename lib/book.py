6b7ca03af697f5d33fa4d3c69ec79174decb105a
Billadams Nyamweno <nyamweno.billadams@gmail.com>
2025-05-12 15:16:09 +0300

    final code

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,52 @@
 #!/usr/bin/env python3
+import sys
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        self._author = ""
+        self._genre = ""
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        self._author = value
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int) and value > 0:
+            self._page_count = value
+        else:
+            
+            print("page_count must be an integer", file=sys.stdout)
+
+    @property
+    def genre(self):
+        return self._genre
+
+    @genre.setter
+    def genre(self, value):
+        self._genre = value
+
+    def summary(self):
+        return f"{self.title} by {self.author}, {self.page_count} pages, Genre: {self.genre}"
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!", file=sys.stdout)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,59 @@
 #!/usr/bin/env python3
+import sys
 
 class Shoe:
-    pass
+    def __init__(self, brand="", size=0, color="", price=0.0):
+        self._brand = brand
+        self._size = size
+        self._color = color
+        self._price = price
+        self._condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, (int, float)) and value > 0:
+            self._size = value
+        else:
+            print("size must be an integer", file=sys.stdout)
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        self._color = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            self._price = value
+        else:
+            raise ValueError("price must be a non-negative number")
+
+    @property
+    def condition(self):
+        return self._condition
+
+    def description(self):
+        return f"{self.color.capitalize()} {self.brand} shoe, size {self.size}, priced at ${self.price:.2f}, condition: {self.condition}"
+
+    def cobble(self):
+        self._condition = "New" 
+        print("Your shoe is as good as new!", file=sys.stdout)
